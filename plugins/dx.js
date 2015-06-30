c464e3ac156cde259ffb43ea7f9172bfdcf3ad44
Alexy Mikhailichenko <alexy@gorges.us>
2015-06-30 13:32:55 -0400

    Use Math.random() properly

--- plugins/dx.js
+++ plugins/dx.js
@@ -7,7 +7,7 @@ module.exports.run = function (client) {
             var last_product_id = 394255;
             var dealextreme_url = 'http://www.dx.com';
 
-            var random_product_id = Math.random(1, last_product_id);
+            var random_product_id = Math.floor(Math.random() * (last_product_id - 500)) + 500;
 
             client.say(channel, 'Random DealExtreme product: ' + dealextreme_url + '/p/' + random_product_id);
 
