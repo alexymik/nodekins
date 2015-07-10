4c98ce39ad34aa797406e27feff56765d0bb4323
Alexy Mikhailichenko <alexy@gorges.us>
2015-07-10 11:01:46 -0400

    Add some additional config checking, user feedback, documentation

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 var fs = require('fs');
 var irc = require('irc');
 
+// Open config.json file
 var config_output = fs.readFileSync('config.json', 'utf8', function (err) {
     if (err) {
-        console.log('Error opening/reading config.json');
+        console.log('Error opening/reading config.json. Rename config-example.json to config.json to use.');
         process.exit(1);
     }
 });
 
+// Parse the config
 config = JSON.parse(config_output);
+
+['server', 'nick'].forEach(function(currentValue) {
+    if (! config[currentValue]) {
+        console.log('Missing config values, check your config.json');
+        process.exit(1);
+    }
+});
 
 var client = new irc.Client(config.server, config.nick, {
     debug: false,
@@ -17,13 +26,14 @@ var client = new irc.Client(config.server, config.nick, {
 
 client.addListener('registered', function(message) {
     // Stuff to do after connecting here
-
     console.log('Connected to ' + message.server)
 });
 
-config.plugins.forEach(function(currentValue) {
-    require('./plugins/' + currentValue).run(client);
-});
+if (config.plugins) {
+    config.plugins.forEach(function(currentValue) {
+        require('./plugins/' + currentValue).run(client);
+    });
+}
 
 client.addListener('message', function(nick, channel, message) {
     params = message.split(' ');
