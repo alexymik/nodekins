90df70613c5f6d7d9c33e1621462d0811e760f76
Alexy Mikhailichenko <alexymik@users.noreply.github.com>
2016-03-18 11:05:23 -0400

    Update DX plugin to keep trying if it gets a 404

--- plugins/dx.js
+++ plugins/dx.js
@@ -1,17 +1,32 @@
+var rp = require('request-promise');
+
 module.exports.run = function (client) {
+
+    function get_random_dx_product_url() {
+        var last_product_id = 394255;
+        var dealextreme_url = 'http://www.dx.com';
+        var random_product_id = Math.floor(Math.random() * (last_product_id - 500)) + 500;
+
+        return dealextreme_url + '/p/' + random_product_id;
+    }
+
+    function get_dx_product(channel) {
+
+        var dx_url = get_random_dx_product_url();
+
+        rp(dx_url).then(function() {
+            client.say(channel, 'Random DealExtreme product: ' + dx_url);
+        }).catch(function() {
+            get_dx_product(channel);
+        });
+
+    }
+
     client.addListener('message', function(nick, channel, message) {
-        var params = message.split(' ');
+        params = message.split(' ');
 
         if (params[0] == '.dx') {
-
-            var last_product_id = 394255;
-            var dealextreme_url = 'http://www.dx.com';
-
-            var random_product_id = Math.floor(Math.random() * (last_product_id - 500)) + 500;
-
-            client.say(channel, 'Random DealExtreme product: ' + dealextreme_url + '/p/' + random_product_id);
-
-            // TODO: Check if the URL is a 404, generate a new one before returning.
+            get_dx_product(channel);
         }
 
     });
